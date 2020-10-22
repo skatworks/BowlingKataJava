f15ea5ef81c52a2c97c86bd46d38f7d36b62f326
s.kogawa <s.kogawa@sbfw.co.jp>
2020-10-23 01:10:58 +0900

    テストケースとテスト対象クラスを作成、ガターの場合のテストを作成

---
+++ src/main/java/jp/co/solver/java/BowlingKata/BowlingGame.java
@@ -0,0 +1,13 @@
+package jp.co.solver.java.BowlingKata;
+
+public class BowlingGame {
+
+	public void roll(int i) {
+
+	}
+
+	public int score() {
+		return 0;
+	}
+
+}
---
+++ src/test/java/jp/co/solver/java/BowlingKata/BowlingGameTest.java
@@ -0,0 +1,19 @@
+package jp.co.solver.java.BowlingKata;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void testGutterGame() {
+		BowlingGame g = new BowlingGame();
+		for (int i = 0; i < 20; i ++) {
+			g.roll(0);
+		}
+		assertThat(g.score(), is(0));
+	}
+
+}
