2b8588e56ef6868110b095f31bfe6b06961a3195
s.kogawa <s.kogawa@sbfw.co.jp>
2020-10-23 01:57:53 +0900

    スペアの場合のテストを作成、テスト対象をスペアの場合に合致するよう修正

--- src/main/java/jp/co/solver/java/BowlingKata/BowlingGame.java
+++ src/main/java/jp/co/solver/java/BowlingKata/BowlingGame.java
@@ -2,13 +2,25 @@ package jp.co.solver.java.BowlingKata;
 
 public class BowlingGame {
 
-	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
 
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
+		int score = 0;
+		int i = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if (rolls[i] + rolls[i + 1] == 10) {
+				score += 10 + rolls[i + 2];
+				i += 2;
+			} else {
+				score += rolls[i] + rolls[i + 1];
+				i += 2;
+			}
+		}
 		return score;
 	}
 
--- src/test/java/jp/co/solver/java/BowlingKata/BowlingGameTest.java
+++ src/test/java/jp/co/solver/java/BowlingKata/BowlingGameTest.java
@@ -3,25 +3,43 @@ package jp.co.solver.java.BowlingKata;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
+	private BowlingGame g;
+
+	@Before
+	public void setUp() throws Exception {
+		g = new BowlingGame();
+	}
+
 	@Test
-	public void testGutterGame() {
-		BowlingGame g = new BowlingGame();
-		for (int i = 0; i < 20; i ++) {
-			g.roll(0);
-		}
+	public void testGutterGame() throws Exception {
+		rollMany(20, 0);
 		assertThat(g.score(), is(0));
 	}
 
 	@Test
-	public void testAllOnes() {
-		BowlingGame g = new BowlingGame();
-		for (int i = 0; i < 20; i++) {
-			g.roll(1);
-		}
+	public void testAllOnes() throws Exception {
+		rollMany(20, 1);
 		assertThat(g.score(), is(20));
+	}
+
+	@Test
+	public void testOneSpare() throws Exception {
+		g.roll(5);
+		g.roll(5);
+		g.roll(3);
+		rollMany(17,0);
+		assertThat(g.score(), is(16));
+	}
+
+	// メソッド抽出
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++) {
+			g.roll(pins);
+		}
 	}
 }
