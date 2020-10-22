79ed42aa4b27cf56950637f9e059a48d6ee551b0
s.kogawa <s.kogawa@sbfw.co.jp>
2020-10-22 23:43:42 +0900

    ガター、スペア、ストライク、パーフェクトまで対応。

--- src/main/java/jp/co/solver/java/BowlingKata/Game.java
+++ src/main/java/jp/co/solver/java/BowlingKata/Game.java
@@ -1,14 +1,34 @@
 package jp.co.solver.java.BowlingKata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
-	private int score = 0;
+	private List<Integer> rolls = new ArrayList<Integer>();
 
 	public void roll(int pins) {
-		score = score + pins;
+		rolls.add(pins);
 	}
 
 	public int score() {
+		int score = 0;
+		int rollIdx = 0;
+		for (int i = 0; i < 10; i++) {
+			if (rolls.get(rollIdx) == 10) {
+				int next = ((rollIdx + 1) < rolls.size()) ? rolls.get(rollIdx + 1) : 0;
+				int nextNext = ((rollIdx + 2) < rolls.size()) ? rolls.get(rollIdx + 2) : 0;
+				score += 10 + next + nextNext;
+				rollIdx += 1;
+			} else if (rolls.get(rollIdx) + rolls.get(rollIdx + 1) == 10) {
+				int next = ((rollIdx + 2) < rolls.size()) ? rolls.get(rollIdx + 2) : 0;
+				score += 10 + next;
+				rollIdx += 2;
+			} else {
+				score += rolls.get(rollIdx) + rolls.get(rollIdx + 1);
+				rollIdx += + 2;
+			}
+		}
 		return score;
 	}
 
--- src/test/java/jp/co/solver/java/BowlingKata/GameTest.java
+++ src/test/java/jp/co/solver/java/BowlingKata/GameTest.java
@@ -11,8 +11,7 @@ public class GameTest {
 	public void 全てガター() {
 		Game game = new Game();
 		for (int i = 0; i < 20; i++) {
-			// すべてのフレームで0
-			game.roll(0);
+			rollGutter(game);
 		}
 		assertThat(game.score(), is(0));
 	}
@@ -26,4 +25,65 @@ public class GameTest {
 		assertThat(game.score(), is(20));
 	}
 
+	@Test
+	public void ストライクを含む場合() {
+		Game game = new Game();
+		// 1フレーム目でストライク
+		game.roll(10);
+		// 3、4フレーム目でスペアならず
+		game.roll(3);
+		game.roll(4);
+		// 以降全てのフレームでガター
+		for (int i = 0; i < 18; i++) {
+			game.roll(0);
+		}
+		assertThat(game.score(), is(24));
+	}
+
+	@Test
+	public void スペアを含む場合() {
+		Game game = new Game();
+		rollSpare(game);
+		game.roll(4);
+		game.roll(3);
+		for (int i = 0; i < 16; i++) {
+			rollGutter(game);
+		}
+		assertThat(game.score(), is(21));
+	}
+
+	@Test
+	public void パーフェクトの場合() {
+		Game game = new Game();
+		for (int i = 0; i < 12; i++) {
+			rollStrike(game);
+		}
+		assertThat(game.score(), is(300));
+	}
+
+	@Test
+	public void 受入テスト() {
+		Game game = new Game();
+		int[] pins = new int[] {1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6};
+		for (int pin : pins) {
+			game.roll(pin);
+		}
+		assertThat(game.score(), is(133));
+	}
+
+	// すべてガターの場合のメソッド
+	private void rollGutter(Game game) {
+		game.roll(0);
+	}
+
+	// スペアのメソッド
+	private void rollSpare(Game game) {
+		game.roll(5);
+		game.roll(5);
+	}
+
+	// すべてストライクの場合のメソッド
+	private void rollStrike(Game game) {
+		game.roll(10);
+	}
 }
