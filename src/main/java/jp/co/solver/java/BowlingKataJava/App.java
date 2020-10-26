e509636118dd201c3bd5362998a50cfca071b7b2
s.kogawa <s.kogawa@sbfw.co.jp>
2020-10-26 10:16:02 +0900

    プロジェクト作成。

---
+++ src/main/java/jp/co/solver/java/BowlingKataJava/App.java
@@ -0,0 +1,13 @@
+package jp.co.solver.java.BowlingKataJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
